ead67e8b2755d70fa7e75b1e31a2cfcdc4e71146
jgarciacondado <jorgegarciacondado@gmail.com>
2024-12-19 10:49:54 -0500

    Add HABS PET data

--- scripts/preprocess_HABS.py
+++ scripts/preprocess_HABS.py
@@ -1,56 +1,79 @@
 import pandas as pd
 
 # Load features but ignore first column
-features = pd.read_csv('data/HABS/raw/raw_features.csv', index_col='SubjIDshort')
+df_structural = pd.read_csv('data/HABS/raw/raw_features.csv', index_col='SubjIDshort')
 
 # Remove MRI_FS6_ADNI_ from column names
-features.columns = features.columns.str.replace('MRI_FS6_ADNI_', '')
+df_structural.columns = df_structural.columns.str.replace('MRI_FS6_ADNI_', '')
 
 # Combine those with lh and rh 
-lh = features.filter(like='lh_').columns
-rh = features.filter(like='rh_').columns
+lh = df_structural.filter(like='lh_').columns
+rh = df_structural.filter(like='rh_').columns
 for l, r in zip(lh, rh):
-    features['bi_' + l.split('_')[1]] = features[l] + features[r]
-    features.drop(columns=[l, r], inplace=True)
+    df_structural['bi_' + l.split('_')[1]] = df_structural[l] + df_structural[r]
+    df_structural.drop(columns=[l, r], inplace=True)
 
 # Combine those with Left and Right
-left = features.filter(like='Left').columns
-right = features.filter(like='Right').columns
+left = df_structural.filter(like='Left').columns
+right = df_structural.filter(like='Right').columns
 for l, r in zip(left, right):
-    features['bi_' + l.split('_')[1]] = features[l] + features[r]
-    features.drop(columns=[l, r], inplace=True)
+    df_structural['bi_' + l.split('_')[1]] = df_structural[l] + df_structural[r]
+    df_structural.drop(columns=[l, r], inplace=True)
 
+# Normalize volumes by total intracranial volume
 def normalize_mri(mri_col, icv_col):
     from sklearn.linear_model import LinearRegression
     b = LinearRegression().fit(icv_col.values.reshape(-1,1), mri_col).coef_[0]
     adj_mri = mri_col - b * (icv_col - icv_col.mean())
     return adj_mri
-# Normalize volumes by total intracranial volume
-features['bi_Hippocampus'] = normalize_mri(features['bi_Hippocampus'], features['ICV_vol'])
-features['bi_Amygdala'] = normalize_mri(features['bi_Amygdala'], features['ICV_vol'])
+df_structural['bi_Hippocampus'] = normalize_mri(df_structural['bi_Hippocampus'], df_structural['ICV_vol'])
+df_structural['bi_Amygdala'] = normalize_mri(df_structural['bi_Amygdala'], df_structural['ICV_vol'])
 
 # Keep only MRI_Age and bi_ columns
-features = features[['MRI_Age'] + [col for col in features.columns if 'bi_' in col]]
+df_structural = df_structural[['MRI_Age'] + [col for col in df_structural.columns if 'bi_' in col]]
 
 # Rename MRI_Age to age and lower case column names
-features = features.rename(columns={'MRI_Age': 'age'})
-features.columns = features.columns.str.lower()
+df_structural = df_structural.rename(columns={'MRI_Age': 'age'})
+df_structural.columns = df_structural.columns.str.lower()
 
 # Save as csv
-features.to_csv('data/HABS/processed/structural_features.csv')
+df_structural.to_csv('data/HABS/processed/structural_features.csv')
 
-# Load A4 data 
-df_a4 = pd.read_csv('data/A4/structural_features.csv', index_col='BID')
-df_a4.columns = df_a4.columns.str.lower()
+# Load A4 structural data 
+df_a4_structural = pd.read_csv('data/A4/structural_features.csv', index_col='BID')
+df_a4_structural.columns = df_a4_structural.columns.str.lower()
 
 # Stack dataframes
-df = pd.concat([features, df_a4], axis=0)
-df.index.name = 'SubjIDshort'
-df.to_csv('data/HABS/processed/HABS_A4_structural_features.csv')
+df_structural = pd.concat([df_structural, df_a4_structural], axis=0)
+df_structural.index.name = 'SubjIDshort'
+df_structural.to_csv('data/HABS/processed/HABS_A4_structural_features.csv')
 
 # Create new dataframe with CN column with 1 for those in a4 and 0 for those in HABS
-df['cn'] = df.index.isin(df_a4.index).astype(int)
-clinical = df[['cn']].copy()
+df_structural['cn'] = df_structural.index.isin(df_a4_structural.index).astype(int)
+clinical = df_structural[['cn']].copy()
 clinical['HABS'] = (~clinical['cn'].astype(bool)).astype(int)
-df.drop(columns='cn', inplace=True)
+df_structural.drop(columns='cn', inplace=True)
 clinical.to_csv('data/HABS/processed/A4_as_cn.csv')
+
+# Load PET data
+df_pet = pd.read_csv('data/HABS/raw/raw_pet_features.csv', index_col='SubjIDshort')
+df_pet.columns = df_pet.columns.str.replace('TAU_FS_SUVR_PVC_', '')
+df_pet = df_pet.rename(columns={'TAU_Age': 'age'})
+df_pet.columns = df_pet.columns.str.lower()
+df_pet = df_pet[['age'] + [col for col in df_pet.columns if '_bh' in col]]
+
+# Remove outlier
+outliers = ['B_RAUOBJ']
+
+# Save as csv
+df_pet.to_csv('data/HABS/processed/pet_features.csv')
+
+# Load A4 PET data
+df_a4_pet = pd.read_csv('data/A4/tau_features.csv', index_col='BID')
+df_a4_pet.columns = df_a4_pet.columns.str.lower()
+df_a4_pet.columns = ['_'.join(col.split('_')[1:]) + '_bh' if 'bi_' in col else col for col in df_a4_pet.columns]
+
+# Stack dataframes
+df_pet = pd.concat([df_pet, df_a4_pet], axis=0)
+df_pet.index.name = 'SubjIDshort'
+df_pet.to_csv('data/HABS/processed/HABS_A4_pet_features.csv')
